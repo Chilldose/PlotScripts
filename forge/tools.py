897985ac6ba0ffbec3dafb9c9996413700a0a639
Dominic Blöch <dominic.bloech@oeaw.ac.at>
2020-04-21 16:44:28 +0200

    Custom importer example

--- forge/tools.py
+++ forge/tools.py
@@ -510,9 +510,34 @@ def read_in_files(filepathes, configs):
             return read_in_CSV_measurement_files(filepathes)
         elif filetype.upper() == "CUSTOM":
             if custom_specs:
-                data = read_in_CUSTOM_measurement_files(filepathes, custom_specs)
-                if data and isinstance(data, dict):
-                    return data, []
+                data_raw = read_in_CUSTOM_measurement_files(filepathes, custom_specs)
+                if data_raw and isinstance(data_raw, dict):
+                    # Add the necessary data structure
+                    final_data = {}
+                    for key, value in data_raw.items():
+                        try:
+                            if isinstance(value, dict):
+                                processed_data = {"analysed": False, "plots":False}
+                                processed_data["data"] = value["data"]
+                                if "measurements" not in value:
+                                    processed_data["measurements"] = list(value["data"].keys())
+                                else: processed_data["measurements"] = value["measurements"]
+                                if "units" not in value:
+                                    processed_data["units"] = ["arb. units" for i in processed_data["measurements"]]
+                                else:
+                                    processed_data["units"] = value["units"]
+                                if "header" not in value:
+                                    processed_data["header"] = ""
+                                else:
+                                    processed_data["header"] = value["header"]
+                                if "additional" in value:
+                                    processed_data["additional"] = value["additional"]
+                                final_data[key] = processed_data
+                            else:
+                                log.error("Data format for custom data array {} is not dict. Discarding data.".format(key))
+                        except Exception as err:
+                            log.error("An error happened during parsind data from CUSTOM importer output. Most likely the outpot did not had the correct form. Error: {}".format(err))
+                    return final_data, []
                 else:
                     log.error("Return data from CUSTOM file parsing did not yield valid data. Data must be a dictionary!")
                     return {}, []
@@ -529,8 +554,9 @@ def read_in_files(filepathes, configs):
             filename, file_extension = os.path.splitext(file)
             if file_extension.lower() == ".txt" or file_extension.lower == ".dat":
                 if ascii_specs:
-                    data, load = read_in_ASCII_measurement_files([file], ascii_specs)
-                    data.update(data)
+                    data_new, load = read_in_ASCII_measurement_files([file], ascii_specs)
+                    data.update(data_new)
+                    load_order.append(load)
                 else:
                     log.error("ASCII file type files must be given with specifications how to interpret data.")
             elif file_extension.lower() == ".json" or file_extension.lower == ".yml" or file_extension.lower == ".yaml":
